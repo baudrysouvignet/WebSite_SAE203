abe511edfeedf0602426b195dc2dcd0e7f5ab270
Baudry Souvignet <76646601+baudrysouvignet@users.noreply.github.com>
2023-03-17 21:06:04 +0100

    Days 1 part 1 Header OK BDD ok Pages en cours -> les ecrivains a droites

--- pages.php
+++ pages.php
@@ -4,46 +4,58 @@ include 'source/php_request_header.php'; // Requête pour le header et importati
 include 'source/function.php'; // Importation de fonctions
 
 // Redirection si aucun ID n'est spécifié dans l'URL
-if (!isset($_GET['id'])) {
-    header('Location: pages.php?id='.$tab[0]["id_tag"]);
+if ( ! isset( $_GET[ 'id' ] ) ) {
+    header ( 'Location: pages.php?id=' . $tab[ 0 ][ "id_tag" ] );
     exit();
 }
 
 // Gestion du système de tri des données
-$filtres = [1=>'Les plus vues', 2=> 'Nouveauté', 3=>'Notre selection'];
+$filtres = [1 => 'Les plus vues' , 2 => 'Nouveauté' , 3 => 'Notre selection'];
 $groupeby = 'ORDER BY Vues';
 $trie = 1;
-if (isset($_GET['trie'])){
-    $trie = $_GET['trie'];
+if ( isset( $_GET[ 'trie' ] ) ) {
+    $trie = $_GET[ 'trie' ];
 
     // Selon la valeur de $_GET['trie'], on modifie la clause ORDER BY de la requête SQL
-    switch ($trie){
+    switch ($trie) {
         case (1):
-            $groupeby = 'ORDER BY Vues DESC'; break;
+            $groupeby = 'ORDER BY Vues DESC';
+            break;
         case (2):
-            $groupeby = 'ORDER BY date DESC'; break;
+            $groupeby = 'ORDER BY date DESC';
+            break;
         case (3):
-            $groupeby = 'ORDER BY RAND ()'; break;
+            $groupeby = 'ORDER BY RAND ()';
+            break;
     }
 }
 
 // Requête SQL pour récupérer le nom de la page associé au tag
-$requete_prepare = $bdd->prepare('SELECT nom FROM Tag WHERE id_tag=:tag_id');
-$requete_prepare->bindValue(':tag_id', $_GET['id'], PDO::PARAM_INT);
-$requete_prepare->execute();
-$res_nom = $requete_prepare->fetch(PDO::FETCH_ASSOC);
+$requete_prepare = $bdd->prepare ( 'SELECT nom FROM Tag WHERE id_tag=:tag_id' );
+$requete_prepare->bindValue ( ':tag_id' , $_GET[ 'id' ] , PDO::PARAM_INT );
+$requete_prepare->execute ();
+$res_nom = $requete_prepare->fetch ( PDO::FETCH_ASSOC );
+$requete_prepare -> closeCursor ();
+
+// Requête SQL pour récupérer les tag assoicé
+$requete_prepare = $bdd->prepare ( 'SELECT tag.id_tag, Tag.nom FROM tag, Articles, Vconnect WHERE Tag.id_tag = Vconnect.id_tag AND Articles.id_article = Vconnect.id_article AND tag.id_tag != :tag_id ORDER BY RAND () LIMIT 10;' );
+$requete_prepare->bindValue ( ':tag_id' , $_GET[ 'id' ] , PDO::PARAM_INT );
+$requete_prepare->execute ();
+$tab_tag_asso = $requete_prepare->fetchAll ( PDO::FETCH_ASSOC );
+$requete_prepare -> closeCursor ();
 
 // Requête SQL pour récupérer les articles associés au tag, avec le nombre de vues
-$requete_prepare = $bdd->prepare('SELECT DISTINCT COUNT(DISTINCT vues.ip) as vues, Articles.id_article, Articles.titre, Articles.contenue, Articles.img, UPPER(Ecrivains.nom) AS nom, Ecrivains.prenom, Articles.date 
+$requete_prepare = $bdd->prepare ( 'SELECT DISTINCT COUNT(DISTINCT vues.ip) as vues, Articles.id_article, Articles.titre, Articles.contenue, Articles.img, UPPER(Ecrivains.nom) AS nom, Ecrivains.prenom, Articles.date 
 FROM Articles
 LEFT JOIN vues ON Articles.id_article = vues.vue_id_article
 JOIN Ecrivains ON Articles.article_id_ecrivain = Ecrivains.id_ecrivains 
 JOIN Vconnect ON Articles.id_article = Vconnect.id_article 
 JOIN Tag ON Vconnect.id_tag = Tag.id_tag AND Tag.id_tag = :tag_article_id
-GROUP BY Articles.id_article '. $groupeby);
-$requete_prepare->bindValue(':tag_article_id', $_GET['id'], PDO::PARAM_INT);
-$requete_prepare->execute();
-$tab_acrticles = $requete_prepare->fetchAll(PDO::FETCH_ASSOC);
+GROUP BY Articles.id_article ' . $groupeby );
+$requete_prepare->bindValue ( ':tag_article_id' , $_GET[ 'id' ] , PDO::PARAM_INT );
+$requete_prepare->execute ();
+$tab_acrticles = $requete_prepare->fetchAll ( PDO::FETCH_ASSOC );
+$requete_prepare -> closeCursor ();
 
 ?>
 
@@ -64,7 +76,7 @@ $tab_acrticles = $requete_prepare->fetchAll(PDO::FETCH_ASSOC);
 
 <div class="content">
     <div class="head">
-        <h1 class="big_text"><span class="color">#</span><?php echo $res_nom['nom'] ?></h1>
+        <h1 class="big_text"><span class="color">#</span><?php echo $res_nom[ 'nom' ] ?></h1>
         <a href="rediger.php" class="button">Ecrire un article</a>
     </div>
 
@@ -72,10 +84,10 @@ $tab_acrticles = $requete_prepare->fetchAll(PDO::FETCH_ASSOC);
 
         <?php
         // Boucle pour générer les liens HTML
-        foreach ($filtres as $key => $val){
+        foreach ( $filtres as $key => $val ) {
             // Détermination de l'attribut href et class
-            $href = compare($key, $trie, '', "href='pages.php?id=".$_GET["id"]."&trie=".$key."'"); /*permet de comparer les valeur et de retourner les valeurs voulu grace a la fonction*/
-            $class = compare ($key, $trie, 'actif', 'inactif');
+            $href = compare ( $key , $trie , '' , "href='pages.php?id=" . $_GET[ "id" ] . "&trie=" . $key . "'" ); /*permet de comparer les valeur et de retourner les valeurs voulu grace a la fonction*/
+            $class = compare ( $key , $trie , 'actif' , 'inactif' );
 
             // Affichage du lien HTML
             echo <<<HTML
@@ -89,16 +101,16 @@ HTML;
     <section>
         <?php
         // boucle qui itère sur chaque article à afficher
-        foreach ($tab_acrticles as $article){
+        foreach ( $tab_acrticles as $article ) {
             // formatage du prénom de l'auteur avec une majuscule en début de chaîne
-            $prenom = ucfirst($article["prenom"]);
+            $prenom = ucfirst ( $article[ "prenom" ] );
             // formatage de la date au format jj/mm/aaaa
-            $date_fr = date('d/m/Y', strtotime($article["date"]));
+            $date_fr = date ( 'd/m/Y' , strtotime ( $article[ "date" ] ) );
 
             // affichage de chaque article avec les informations correspondantes
             echo <<<HTML
 <article>
-    <img src="{$article["img"]}" alt="l'image decris {$article["titre"]}">
+    <img src="img/{$article["img"]}" alt="l'image decris {$article["titre"]}">
     <div class="info">
         <span>Redigé par {$article["nom"]} $prenom - $date_fr</span>
         <h2>{$article["titre"]}</h2>
@@ -111,6 +123,41 @@ HTML;
 
         ?>
     </section>
+
+
+    <aside>
+        <div class="state">
+            <div>
+                <i class="fa-solid fa-eye"></i>
+                <span><?php
+                    $somme = 0;
+
+                    foreach ($tab_acrticles as $article) {
+                        $somme += $article['vues'];
+                    }
+                    echo $somme
+                    ?></span>
+            </div>
+            <div>
+                <i class="fa-solid fa-newspaper"></i>
+                <span><?php echo count ($tab_acrticles)?></span>
+            </div>
+        </div>
+
+        <h2>Meilleurs <span class="color">tag</span> associé</h2>
+        <div class="tag">
+            <?php
+            foreach ($tab_tag_asso as $value){
+                echo <<<HTML
+                <a href='pages.php?id={$value['id_tag']}' class="button"><span class="color">#</span>{$value['nom']}</a>
+                HTML;
+
+            }
+
+            ?>
+        </div>
+    </aside>
+
 </div>
 
 
