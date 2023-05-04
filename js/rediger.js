aa2c5adb434df490bdf05fd204cf8b0c1907ea67
Okter <contact.okter@gmail.com>
2023-05-04 09:42:54 +0200

    Car ok

--- js/rediger.js
+++ js/rediger.js
@@ -1,35 +1,50 @@
+/*---------------------------------------------------------------------------------------------------------
+Ce code Js est nécessaire a l'ux design lors de la partie 1, il ne fait donc pas partie des 3 intégrations
+---------------------------------------------------------------------------------------------------------*/
+
 /*déclarations des variables*/
 var elements = document.querySelectorAll('.filtre, #affiche');
 const filtre = document.querySelector('.filtre');
 const popup = document.querySelector('#popup');
 
-// Fonction pour ajouter ou enlever la classe "unactive"
+/**
+ * Fonction pour changer les class
+ */
 function toggleUnactive() {
     filtre.classList.toggle('unactive');
     popup.classList.toggle('unactive');
 }
 
-// ajoute un écouteur d'événements "click" à chaque élément sélectionné
+/**
+ * ajoute un écouteur d'événements "click" à chaque élément sélectionné
+ */
 elements.forEach(function(element) {
     element.addEventListener('click', function() {
-        // vérifie si l'élément a la classe "unactive"
+        /*vérifie si l'élément a la classe "unactive"*/
         if (this.classList.contains('unactive')) {
-            // si l'élément a la classe "unactive", la supprime
+            /*si l'élément a la classe "unactive", la supprime*/
             toggleUnactive()
         } else {
-            // sinon, ajoute la classe "unactive"
+            /*sinon, ajoute la classe "unactive"*/
             toggleUnactive()
         }
     });
 });
 
 
-
+/**
+ * Affiche un shamp de formulaire si le select est sur 'autre'
+ *
+ * @param type
+ */
 function showInput(type) {
     const select = document.getElementById("select_"+type);
     const label = document.getElementById("label_select_"+type);
     const inputContainer = document.getElementById("input-container_"+type);
+
+    /*verification si la valeur est autre*/
     if (select.value === "autre") {
+        /*on fait apparaitre les éléments*/
         inputContainer.style.display = "block";
         select.style.display = "none";
         label.style.display = "none";
