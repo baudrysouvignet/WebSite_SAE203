ce4767998f51502d26cce55c8c75796700652105
Okter <contact.okter@gmail.com>
2023-05-10 10:54:02 +0200

    JS ++ new config

--- config/connection.php
+++ config/connection.php
@@ -11,5 +11,15 @@
     //Connection en PDO a la BDD new203 (réglage Mac mdp: root)
     $bdd = new PDO('mysql:host='.$hote.';port='.$port.';dbname='.$nom_bd,$identifiant, $mot_de_passe,$options);*/
 
+    // modifiez les paramètres pour accéder à la BDD client
+    $hote='localhost';
+    $port='4444';
+    $nom_bd='new203';
+    $identifiant='root';
+    $mot_de_passe='root';
+    $encodage='utf8';
+    $options=array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES '.$encodage);
 
+    //Connection en PDO a la BDD new203 (réglage Mac mdp: root)
+    $bdd = new PDO('mysql:host='.$hote.';port='.$port.';dbname='.$nom_bd,$identifiant, $mot_de_passe,$options);
 ?>
